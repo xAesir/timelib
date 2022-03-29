84bc78d9364e780f70aa7532e95ff6f7338208b4
xAesir <mail.aesir@gmail.com>
2022-03-29 11:59:34 +0200

    Einfügen des eigenen Codes

--- main.c
+++ main.c
@@ -1,20 +1,32 @@
-/**
- *
- *
- *
- **/
-
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- *
- *
- **/
+int leapYearCheck(int year){
+    if ((year & 3) == 0 && ((year % 25) != 0 || (year & 15) == 0)){
+        return 1;
+    }
+    return 0;
+}
+
 int day_of_the_year(int day, int month, int year)
 {
 
-    return 0;
+    int sumOfDays = 0;
+
+    int daysInMonths[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+    if (leapYearCheck(year))
+    {
+       daysInMonths[1] = 29;
+    }
+
+    for (int i = 0; i < month-1; i++) {
+        sumOfDays += daysInMonths[i];
+    }
+
+    sumOfDays += day;
+    
+    printf("Der %i. %i. %i war der %i. Tag des Jahres.", day, month, year, sumOfDays);
 }
 
 int main()
