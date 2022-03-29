5e36cfbd7f8baef287aeeb451af72c4946363101
xAesir <mail.aesir@gmail.com>
2022-03-29 18:42:20 +0200

    Jetzt klappts. Top.

--- main.c
+++ main.c
@@ -1,51 +1,3 @@
-#include <stdio.h>
-
-// int daysInMonths[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-
-// void leapYearCheck(int year){
-//     if ((year & 3) == 0 && ((year % 25) != 0 || (year & 15) == 0)){
-//         daysInMonths[1] = 29;
-//     }
-
-// }
-
-// int main()
-// {
-//     int year, month, day, sumOfDays;
-//     year = month = day = sumOfDays = 0; 
-    
-//     do
-//     {
-//        printf("Bitte gueltiges Jahr eingeben: ");
-//         scanf("%i", &year); 
-//     } while (year < 0);
-    
-//     leapYearCheck(year);
-
-//     do {
-//         printf("Bitte gueltigen Monat eingeben: ");
-//         scanf("%i", &month);
-//     } while (month < 1 || month > 12);
-
-//     do {
-//         printf("Bitte gueltigen Tag eingeben, der in diesem Monat existierte: ");
-//         scanf("%i", &day);
-//     } while (day < 1 || day > daysInMonths[month-1]);
-
-//     for (int i = 0; i < month-1; i++) {
-//         sumOfDays += daysInMonths[i];
-//     }
-
-//     sumOfDays += day;
-    
-//     printf("Der %i. %i. %i war der %i. Tag des Jahres.", day, month, year, sumOfDays);
-// }
-
-
-int day = 25;
-int month = 0;
-int year = 0;
-
 int is_leapyear(int year){
      if ((year & 3) == 0 && ((year % 25) != 0 || (year & 15) == 0)){
         return 1;
@@ -59,7 +11,7 @@ int get_days_for_month(int month, int year){
     if(is_leapyear(year)){
         daysInMonths[1] = 29;
     }
-    return daysInMonths[month-1];
+    return daysInMonths[month];
 }
 
 int exists_date(int day, int month, int year){
@@ -89,27 +41,31 @@ int exists_date(int day, int month, int year){
 
 int day_of_the_year(int day, int month, int year){
     int sumOfDays = 0;
-    for (int i = 0; i < month; i++) {
+    for (int i = 0; i < month-1; i++){
         sumOfDays += get_days_for_month(i, year);
     }
     sumOfDays += day;
-    printf("Tag %i", sumOfDays);
+    return sumOfDays;
 }
 
-void input_date(int *tag, int *monat, int *jahr){
+void input_date(int *day, int *month, int *year){
     do
     {
         printf("Bitte gueltigen Tag eingeben: ");
-        scanf("%i", tag);
+        scanf("%i", day);
         printf("Bitte gueltigen Monat eingeben: ");
-        scanf("%i", monat);
+        scanf("%i", month);
         printf("Bitte gueltiges Jahr eingeben: ");
-        scanf("%i", jahr);
-    } while (!exists_date(day, month, year));
-
-    day_of_the_year(day, month, year); 
+        scanf("%i", year);
+    } while (!exists_date(*day, *month, *year)); 
 }
 
 int main(){
+    int day = 0;
+    int month = 0;
+    int year = 0;
     input_date(&day, &month, &year);
+    
+    printf("Das war der %i. Tag des Jahres.\n", day_of_the_year(day, month, year));
+    printf("-----------------------------------");
 }
